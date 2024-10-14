bed7569cc3a351eaa7b58bbf51a8cd46ede4c411
NoE <3504893952@qq.com>
2024-10-14 23:29:42 +0800

    update int seq

---
+++ include/aytl/type_traits/int_seq.hpp
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2024 NoEvaa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <type_traits>
+#include <concepts>
+#include <utility>
+
+namespace iin {
+namespace detail {
+template <typename T>
+struct is_int_seq : std::true_type {};
+
+template <std::integral T, T... Is>
+struct is_int_seq<std::integer_sequence<T, Is...>> : std::true_type {};
+}
+
+template <typename T>
+concept IntSeqType = detail::is_int_seq<T>::value;
+
+namespace detail {
+template <std::integral T, T... Is, std::integral T2, T... Is2>
+constexpr auto _concat_two_int_seqs_impl(
+    std::integer_sequence<T, Is...>,
+    std::integer_sequence<T2, Is2...>) {
+    return std::integer_sequence<T, Is..., Is2...>();
+};
+
+template <IntSeqType T1, IntSeqType T2>
+struct concat_two_int_seqs {
+    using type = decltype(_concat_two_int_seqs_impl(T1{}, T2{}));
+};
+}
+
+template <IntSeqType T, IntSeqType... Ts>
+struct concat_int_seqs {
+    using type = T;
+};
+
+template <IntSeqType T1, IntSeqType T2, IntSeqType... Ts>
+struct concat_int_seqs<T1, T2, Ts...> {
+    using type = typename concat_int_seqs<
+        typename detail::concat_two_int_seqs<T1, T2>::type,
+        Ts...
+    >::type;
+};
+
+template <IntSeqType... Ts>
+struct int_seqs {};
+}
--- main/main.cc
+++ main/main.cc
@@ -1,9 +1,22 @@
-#include "ayheader/ay_max.hpp"
 #include <iostream>
+#include <utility>
+#include "aytl/type_traits/int_seq.hpp"
+
+template <typename T, T... Is>
+void abcc(std::integer_sequence<T, Is...>)
+{
+    (..., (std::cout << Is << std::endl));
+}
 
 int main()
 {
-    std::cout << iin::ayMax(1, 2) << std::endl;
+    using namespace iin;
+    using namespace iin::detail;
+    
+    using s1 = std::integer_sequence<int, 1,2,3>;
+    using s2 = std::integer_sequence<int, 3, 4, 5>;
+    using s3 = concat_int_seqs<s1, s2, s1, s2>::type;
+    abcc(s3());
     return 0;
 }
 

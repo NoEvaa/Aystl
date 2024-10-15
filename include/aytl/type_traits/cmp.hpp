91427cb8496e6dbe8dcc097a517faafc853c2d75
NoE <3504893952@qq.com>
2024-10-15 12:53:11 +0800

    add cmp

---
+++ include/aytl/type_traits/cmp.hpp
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2024 NoEvaa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <type_traits>
+namespace iin {
+enum class CmpOp {
+    kEQ = 0,
+    kNE,
+    kLT,
+    kLE,
+    kGT,
+    kGE
+};
+
+template <CmpOp _op, auto _left, auto _right>
+struct cval_cmp : std::false_type {};
+
+template <auto _left, auto _right>
+requires (_left == _right)
+struct cval_cmp<CmpOp::kEQ, _left, _right> : std::true_type {};
+
+template <auto _left, auto _right>
+requires (_left != _right)
+struct cval_cmp<CmpOp::kNE, _left, _right> : std::true_type {};
+
+template <auto _left, auto _right>
+requires (_left < _right)
+struct cval_cmp<CmpOp::kLT, _left, _right> : std::true_type {};
+
+template <auto _left, auto _right>
+requires (_left <= _right)
+struct cval_cmp<CmpOp::kLE, _left, _right> : std::true_type {};
+
+template <auto _left, auto _right>
+requires (_left > _right)
+struct cval_cmp<CmpOp::kGT, _left, _right> : std::true_type {};
+
+template <auto _left, auto _right>
+requires (_left >= _right)
+struct cval_cmp<CmpOp::kGE, _left, _right> : std::true_type {};
+
+template <CmpOp _op, auto _left, auto _right>
+concept CvalCmp = cval_cmp<_op, _left, _right>::value;
+
+}

584073fe879f482be66e16f61a99b4f29cc8c8ed
NoE <3504893952@qq.com>
2025-02-22 03:55:56 +0800

    update

--- main/main.cc
+++ main/main.cc
@@ -51,18 +51,86 @@ enum class ABC {
     k1 = 1,
 };
 
-struct _AyAnyBuf {
-    uint8_t data_[32];
+class AyAny {
+    enum class _Action {
+        kTypeInfo = 0,
+        kDestroy,
+        kCopy,
+        kMove,
+    };
+    using _act_type = void * (*)(_Action, AyAny const *, void *);
+
+public:
+    using size_type = std::size_t;
+
+    static constexpr size_type kPtrSize     = sizeof(void *);
+    static constexpr size_type kMaxPtrSize  = sizeof(uint64_t);
+    static constexpr size_type kBufByteSize = kMaxPtrSize * 3;
+    static constexpr size_type kBufSize     = kBufByteSize / kPtrSize;
+    static_assert(kPtrSize <= kMaxPtrSize);
+    static_assert((kBufByteSize % kPtrSize) == 0);
+
+private:
+    template <typename _Tp>
+    static void * _castToVoidPtr(_Tp const * __p) noexcept {
+        return const_cast<void *>(static_cast<void const *>(__p));
+    }
+
+    template <typename _Tp>
+    void * _toVoidPtr() const noexcept {
+        if constexpr (sizeof(_Tp) <= kBufByteSize) {
+            return _castToVoidPtr(&m_buf);
+        } else {
+            return _castToVoidPtr(m_buf[0]);
+        }
+    }
+
+    template <typename _Tp>
+    _Tp * _toPtr() const noexcept { return static_cast<_Tp *>(_toVoidPtr<_Tp>()); }
+
+    template <typename _Tp>
+    void _initAct() noexcept {
+        m_act = [](_Action _action, AyAny const * _self, void *) -> void * {
+            switch (_action) {
+            case _Action::kTypeInfo:
+                return _castToVoidPtr(&typeid(_Tp));
+            case _Action::kDestroy: {
+                auto * p_v = _self->_toPtr<_Tp>();
+                if constexpr (sizeof(_Tp) <= kBufSize) {
+                    p_v->~ValT();
+                } else if constexpr (std::is_array_v<_Tp>) {
+                    delete[] p_v;
+                } else {
+                    delete p_v;
+                }
+                return nullptr;
+            }
+            case _Action::kCopy:
+            case _Action::kMove:
+            default:
+                return nullptr;
+            }
+        };
+    }
+
+
+private:
+    void *    m_buf[kBufSize] = {};
+    _act_type m_act = nullptr;
 };
 
 #include <any>
 int main()
 {
-    std::any aa;
+    std::any aa = int(1);
+    auto ddd =  std::any_cast<const int>(aa);
+    std::cout << ddd << std::endl;
     std::cout << sizeof(std::any) << std::endl;
+    std::cout << sizeof(AyAny) << std::endl;
     std::cout << sizeof(void*) << std::endl;
-    std::cout << sizeof(_AyAnyBuf) << std::endl;
-    std::cout << sizeof(std::max_align_t) << std::endl;
+    std::cout << sizeof(std::shared_ptr<int>) << std::endl;
+    std::cout << sizeof(std::weak_ptr<int>) << std::endl;
+    std::cout << sizeof(std::unique_ptr<int>) << std::endl;
     std::cout << alignof(std::max_align_t) << std::endl;
 
     std::cout << getTypeName<std::shared_ptr<std::string const &>>() << std::endl;
